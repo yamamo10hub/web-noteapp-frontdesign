93ae21a584b8e24d78c7dd2a4f4bca032c4a0e3a
ryo-yamamo10 <r.yamamo10@gmail.com>
2022-05-30 16:03:03 +0900

    header footer update

--- func.js
+++ func.js
@@ -1,25 +1,24 @@
 function exeCp (lineno) {
     let tgtline = document.getElementById('label' + lineno).innerText;
     //console.log('label' + lineno);
-    console.log(tgtline);
+    //console.log(tgtline);
     navigator.clipboard.writeText(tgtline);
-    //alert("何かおきました！ " + tgtline);
+    //alert("copydoc" + tgtline);
 }
 
 
 function valueChange(event){
-    //alert("何かおきました！ " + event.currentTarget);
     //alert("何かおきました！ " + event.target);
-    console.log(event);
+    //console.log(event);
     let lineall = document.getElementsByName('hilight');
 
     let hitline = [];
-    console.log(lineall[0].value);
+    //console.log(lineall[0].value);
     //lineall.forEach(chkflag => console.log(chkflag.checked));
     lineall.forEach(chkflag => {
         //console.log(chkflag.checked)
         if (chkflag.checked == true) {
-            console.log(chkflag.id);
+            //console.log(chkflag.id);
             exeCp(chkflag.id);
         }
     });
